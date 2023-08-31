a6f885c755d02026f75b88f4e08719ba8166e326
prabhakar <prabhakaraher13@gmail.com>
2023-08-31 23:59:02 +0530

    init

---
+++ minMax.js
@@ -0,0 +1,13 @@
+// Declare an array
+let arr = [2.4,4,6,8,12,25,13,-25.23];
+
+// Extract minimum number
+let minValue = Math.min(...arr);
+
+// Print minimum value
+console.log(minValue);
+
+// Extract maximum value
+let maxValue = Math.max(...arr);
+
+console.log(maxValue);
